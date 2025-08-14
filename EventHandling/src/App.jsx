a0aeb9cd5633b77a99a61a003ce058e9a8ed8772
Vipransh <vipransh231527@gmail.com>
2025-08-14 18:58:27 +0530

    Event Handler and Props UnderStanding

--- EventHandling/src/App.jsx
+++ EventHandling/src/App.jsx
@@ -3,14 +3,38 @@ import './App.css'
 
 function App() {
 const [Text, SetText] = useState("click Me")
-  function handleClick(){
-    SetText("You Clicked Me MotherFucker")
+  // function handleClick(){
+  //   SetText("You Clicked Me MotherFucker")
+  // }
+
+  function handleInputChage(event){
+    console.log(event.target.value)
+  }
+
+  function handleOnSubmit(event){
+    event.preventDefault() // Prevents the default form submission behavior
+    // You can handle the form submission logic here
+    // For example, you can access the input value like this:
+    // const inputValue = event.target.elements[0].value;
+    // console.log(inputValue);
+    // Or if you want to log the value of the input field:
+    alert("Form submitted");
+    console.log(event.target.value)
   }
   return (
     <div>
-      <button onClick={handleClick}> 
+      {/* <p style={{height:"2rem", border:'2px solid black'}}>  Vo bolegi acha ji and apko bolna hai hanji</p> */}
+
+    <form onSubmit={handleOnSubmit}> 
+      <input type="text" onChange={handleInputChage} />
+      <button type='submit'> Submit</button>
+    </form>
+
+      <button onClick={() => {
+        SetText("You Clicked Me")
+      }}> 
         {Text}
-      </button>
+      </button> 
     </div>
   )
 }
